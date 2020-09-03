df6e7b15d40473d6fdc7a201d282d3196f934a75
Avimael <ing.avimelgr95@gmail.com>
2020-09-02 21:59:16 -0500

    Holy-grail

---
+++ src/index.js
@@ -0,0 +1,16 @@
+
+// require('./styles/main.scss');
+//otra forma de cargar los archivos
+import './styles/main.scss';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+import './static/Logo.png';
+import './static/imagen1.jpg';
+import './static/imagen2.jpg';
+import './static/articulo2.jpg'
+
+
+
+
